f556569c968d0629ae11c82b3b7104350656f40b
sarthakkhandelwal7 <sarthak.k7189@gmail.com>
2019-01-02 17:18:49 +0530

    initial commit

--- stacks/stacks/stacks.py
+++ stacks/stacks/stacks.py
@@ -5,7 +5,6 @@ class Stacks:
         self.end_pointer = 0
         self.space = size
         self.size = size
-        self.cell_data = None 
     def push(self,data):
         if self.space:
             self.sta[self.size - self.space] = data  
@@ -13,16 +12,15 @@ class Stacks:
             self.space-=1
         else:
             print('Stack is full')
-    def pop(self,resp):
+    def pop(self):
         if self.space<self.size:
-            if resp:
-                self.cell_data = self.sta[self.end_pointer] 
+            last = self.sta[self.end_pointer]
             self.sta[self.end_pointer] = None
             self.end_pointer-=1
             self.space+=1
             if self.space==self.size:
                 self.end_pointer+=1
-                
+            return last    
         else:
             print('Stack is empty nothing to pop')
     def __str__(self):
