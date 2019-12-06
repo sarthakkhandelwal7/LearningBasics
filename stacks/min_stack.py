d0e695694ac5268a013bfcab3ae5843a0453430e
sarthakkhandelwal7 <sarthak.k7189@gmail.com>
2019-12-06 19:05:42 +0530

    initial commit

--- stacks/min_stack.py
+++ stacks/min_stack.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class MinStack:
 
     def __init__(self):
@@ -34,40 +33,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-class MinStack:
-
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.min_stack = []
-
-    def push(self, x: int) -> None:
-        self.min_stack.append((x, self.getMin(x)))
-
-    def pop(self) -> None:
-        self.min_stack.pop()
-
-    def top(self) -> int:
-        return self.min_stack[len(self.min_stack) - 1][0]
-
-    def getMin(self, x=None) -> int:
-        if x is not None and (len(self.min_stack) is 0 or self.min_stack[len(self.min_stack) - 1][1] > x):
-            return x
-        else:
-            return self.min_stack[len(self.min_stack) - 1][1]
-
-
-def main():
-    minstack = MinStack()
-    minstack.push(0)
-    minstack.push(1)
-    minstack.push(0)
-    print(minstack.getMin())
-    print(minstack.getMin())
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 4e38df042165dc36b314ad074a4ff6d7b3bb03f6
